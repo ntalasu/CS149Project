673a311e31743e43399347d57882a5870a1b35f6
Naina Talasu <naina.talasu@gmail.com>
2025-11-30 23:04:51 -0800

    Add searchFile and closeFile methods with improved menu

--- FileSystem.c
+++ FileSystem.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Function to get a filename from the user
+void getFilename(char *filename, int size) {
+    printf("Enter filename: ");
+    fgets(filename, size, stdin);
+    size_t len = strlen(filename);
+    if (len > 0 && filename[len - 1] == '\n') {
+        filename[len - 1] = '\0';
+    }
+}
+
 // Function to create a file
 void createFile(char* filename) {
     FILE *fp = fopen(filename, "w");
@@ -9,83 +19,139 @@ void createFile(char* filename) {
         printf("Error creating file!\n");
         return;
     }
-    printf("File %s created successfully.\n", filename);
+    printf("File '%s' created successfully.\n", filename);
     fclose(fp);
 }
 
 // Function to write to a file
 void writeToFile(char* filename, char* content) {
-    FILE *fp = fopen(filename, "a"); // Use 'a' for append mode
+    FILE *fp = fopen(filename, "a");
     if (fp == NULL) {
         printf("Error opening file for writing!\n");
         return;
     }
     fprintf(fp, "%s", content);
-    printf("Content written to %s.\n", filename);
+    printf("Content written to '%s'.\n", filename);
     fclose(fp);
 }
 
-// Function to read from a file
+// Function to read a file
 void readFromFile(char* filename) {
     FILE *fp = fopen(filename, "r");
     if (fp == NULL) {
         printf("Error opening file for reading!\n");
         return;
     }
-    int ch; 
-    printf("Contents of %s:\n", filename);
+    int ch;
+    printf("Contents of '%s':\n", filename);
     while ((ch = fgetc(fp)) != EOF) {
         putchar(ch);
-    }
-    if (ferror(fp)) {
-        perror("Read error");
     }
     printf("\n");
     fclose(fp);
 }
 
+// Search for a file
+void searchFile(char* filename) {
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("File '%s' not found.\n", filename);
+        return;
+    }
+    printf("File '%s' exists!\n", filename);
+    fclose(fp);
+}
+
+// Close a file pointer safely
+void closeFile(FILE *fp) {
+    if (fp == NULL) {
+        printf("No file is currently open.\n");
+        return;
+    }
+    if (fclose(fp) == 0) {
+        printf("File closed successfully.\n");
+    } else {
+        printf("Error closing file!\n");
+    }
+}
+
 int main(void) {
+    int choice;
     char filename[256];
     char content[512];
-    int choice;
-    
-    printf("Enter filename: ");
-    fgets(filename, sizeof(filename), stdin);
-    
-    size_t len = strlen(filename);
-    if (len > 0 && filename[len - 1] == '\n') {
-        filename[len - 1] = '\0';
-    }
-    
+
+    FILE *openFile = NULL;   // Track currently open file
+    char openFilename[256];  // Track name of opened file
+
     while (1) {
         printf("\n--- File Operations Menu ---\n");
         printf("1. Create file\n");
         printf("2. Write to file\n");
         printf("3. Read from file\n");
-        printf("4. Exit\n");
-        printf("Choose an option (1-4): ");
-        scanf("%d", &choice);
-        getchar(); 
-        
+        printf("4. Search file\n");
+        printf("5. Open file (keep open)\n");
+        printf("6. Close open file\n");
+        printf("7. Exit\n");
+        printf("Choose an option (1-7): ");
+
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid input!\n");
+            while (getchar() != '\n'); // clear wrong input
+            continue;
+        }
+        getchar(); // remove newline
+
         switch (choice) {
-            case 1:
+
+            case 1: // Create
+                getFilename(filename, sizeof(filename));
                 createFile(filename);
                 break;
-            case 2:
+
+            case 2: // Write
+                getFilename(filename, sizeof(filename));
                 printf("Enter content to write: ");
                 fgets(content, sizeof(content), stdin);
                 writeToFile(filename, content);
                 break;
-            case 3:
+
+            case 3: // Read
+                getFilename(filename, sizeof(filename));
                 readFromFile(filename);
                 break;
-            case 4:
-                printf("Exiting...\n");
+
+            case 4: // Search
+                getFilename(filename, sizeof(filename));
+                searchFile(filename);
+                break;
+
+            case 5: // Open file (persistent)
+                if (openFile != NULL) {
+                    printf("A file is already open. Close it first.\n");
+                    break;
+                }
+                getFilename(filename, sizeof(filename));
+                openFile = fopen(filename, "r");
+                if (openFile == NULL) {
+                    printf("Error opening file!\n");
+                } else {
+                    strcpy(openFilename, filename);
+                    printf("File '%s' opened successfully.\n", filename);
+                }
+                break;
+
+            case 6: // Close
+                closeFile(openFile);
+                openFile = NULL;
+                break;
+
+            case 7: // Exit
+                if (openFile != NULL) fclose(openFile);
+                printf("Goodbye!\n");
                 return 0;
+
             default:
-                printf("Invalid choice!\n");
+                printf("Invalid option!\n");
         }
     }
-    
-    return 0;
 }
